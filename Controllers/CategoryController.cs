2f55dedfe3c726aeab0f120814667aa75571652e
sarival <sarival@sarival.com.br>
2020-07-07 17:06:31 -0300

    usando contexto no controller e finalizando o crud

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
 using Shop.Models;
 
 namespace Shop.Controllers
@@ -11,33 +14,64 @@ namespace Shop.Controllers
     {
         [HttpGet]
         [Route("")]
-        public async Task<ActionResult<List<Category>>> Get()
+        public async Task<ActionResult<List<Category>>> Get([FromServices]DataContext context)
         {
-            return new List<Category>();
+            try
+            {
+                var lista = await context.Categories.AsNoTracking().ToListAsync();
+                return Ok(lista);             
+            }
+            catch (System.Exception)
+            {
+                return BadRequest(new { message = "Não foi possivel recuperar a lista de categoria" });
+            }
         }
 
         [HttpGet]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> GetById(int id)
+        public async Task<ActionResult<Category>> GetById(int id, [FromServices]DataContext context)
         {
-            return new Category();
+            try
+            {
+                var categoria = await context.Categories.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
+
+                if(categoria == null)
+                    return NotFound();
+
+                return Ok(categoria);        
+            }
+            catch (System.Exception)
+            {
+                return BadRequest(new { message = "Não foi possivel recuperar a categoria" });
+            }
         }
 
         [HttpPost]
         [Route("")]
-        public async Task<ActionResult<Category>> Post([FromBody]Category category)
+        public async Task<ActionResult<Category>> Post([FromBody]Category category, [FromServices]DataContext context)
         {
             if(!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
+            
+            try
+            {
+                context.Categories.Add(category);
+                await context.SaveChangesAsync();
+                return Ok(category);
+                
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possivel criar a categoria" });
+            }
 
-            return Ok(category);
         }
 
         [HttpPut]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> Put(int id, [FromBody]Category category)
+        public async Task<ActionResult<Category>> Put(int id, [FromBody]Category category, [FromServices]DataContext context)
         {
 
             if(category.Id != id)
@@ -50,15 +84,44 @@ namespace Shop.Controllers
                 return BadRequest(ModelState);
             }
             
-
-            return Ok(category);
+            try
+            {
+                // context.Entry<Category>(category).State = EntityState.Modified;
+                context.Categories.Update(category);
+                await context.SaveChangesAsync();
+                return Ok(category);
+                
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possivel atualizar a categoria" });
+            }
         }
 
         [HttpDelete]
-        [Route("id:int")]
-        public async Task<ActionResult<Category>> Delete(int id)
+        [Route("{id:int}")]
+        public async Task<ActionResult<Category>> Delete(int id, [FromServices]DataContext context)
         {
-            return Ok();
+            var modelo = await context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if(modelo == null)
+            {
+                return NotFound(new { message = "Não foi possivel encontrar a categoria" });
+            }
+
+            try
+            {
+                context.Categories.Remove(modelo);
+                await context.SaveChangesAsync();
+                return Ok(new { message = "Categoria removida com sucesso!" });               
+            }
+            catch (System.Exception)
+            {
+                
+                return BadRequest(new { message = "Não foi possivel Deletar a categoria" });
+
+            }
+
         }
     }
 }
